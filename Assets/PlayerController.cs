8cd92ed68479076dff32c5e2e16df70f79f20b56
BenSab97 <61198765+BenSab97@users.noreply.github.com>
2020-02-18 11:01:40 -0500

    This is a test for my real project.
    Test Test Test

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.5f;
+    float h;
+    float v;
+    Vector3 move = Vector3.zero;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+  
+    // Vector3 move = new Vector (0, 0, 0);
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        move.x = h * speed;
+        move.z = v * speed;
+        rb.AddForce(move);
+    }
+}
